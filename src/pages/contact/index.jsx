5f6097ba8ffe36d0b573469fb5154315334bd84f
igorGomes <gomesoliveirasl02@hotmail.com>
2022-06-10 11:08:31 -0300

    m

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Content } from './style'
 import { FaLinkedin, FaFacebook, FaInstagram, FaEnvelope } from 'react-icons/fa'
-import ItemContact from '../../../components/itemcontact'
+import ItemContact from '../../../components/itemContact'
 
 export default function Contact() {
   return (
